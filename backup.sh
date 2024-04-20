a03d5ef93ccee3f4213c289d388c82f11c442292
Tomas Zigo <tomas.zigo@slovanet.sk>
2024-04-20 05:12:09 +0200

    Make NFS server mount dir

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 MNT_DIR="$HOME/.mnt"
 NFS_SERVER_MOUNT_DIR=/nfs-server
 
-mkdir -p "$MNT_DIR"
+mkdir -p "$MNT_DIR" "$NFS_SERVER_MOUNT_DIR"
 goofys gisquick-backup "$MNT_DIR"
 mount -t nfs4 "$NFS_SERVER_ADDRESS":/ $NFS_SERVER_MOUNT_DIR
 tar -czf ./.mnt/qgis-server/backup-qgis-server-publish-"$(date '+%Y-%m-%d')".tar.gz /$NFS_SERVER_MOUNT_DIR/qgis-server-publish
