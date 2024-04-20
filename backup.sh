2166516db53de621bffa9d7045cabce4d9ab65b9
Tomas Zigo <tomas.zigo@slovanet.sk>
2024-04-20 06:37:49 +0200

    Fix tar root AWS S3 bucket mount dir path

--- backup.sh
+++ backup.sh
@@ -6,6 +6,6 @@ NFS_SERVER_MOUNT_DIR=/nfs-server
 mkdir -p "$MNT_DIR" "$NFS_SERVER_MOUNT_DIR"
 goofys gisquick-backup "$MNT_DIR"
 mount -t nfs4 "$NFS_SERVER_ADDRESS":/ $NFS_SERVER_MOUNT_DIR
-tar -czf ./.mnt/qgis-server/backup-qgis-server-publish-"$(date '+%Y-%m-%d')".tar.gz /$NFS_SERVER_MOUNT_DIR/qgis-server-publish
-tar -czf ./.mnt/qgis-server/backup-qgis-server-plugin-"$(date "+%Y-%m-%d")".tar.gz /$NFS_SERVER_MOUNT_DIR/qgis-server-plugin
+tar -czf "$MNT_DIR"/qgis-server/backup-qgis-server-publish-"$(date '+%Y-%m-%d')".tar.gz /$NFS_SERVER_MOUNT_DIR/qgis-server-publish
+tar -czf "$MNT_DIR"/qgis-server/backup-qgis-server-plugin-"$(date "+%Y-%m-%d")".tar.gz /$NFS_SERVER_MOUNT_DIR/qgis-server-plugin
 umount "$MNT_DIR"
