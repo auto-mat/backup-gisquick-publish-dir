b83c97a18d24d5aaed19965a765db3b66795a9e1
Tomas Zigo <tomas.zigo@slovanet.sk>
2024-05-06 20:56:17 +0200

    Fix S3 bucket backup dir path

--- backup.sh
+++ backup.sh
@@ -10,8 +10,9 @@ aws_access_key_id = $AWS_ACCESS_KEY_ID
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
 " > "$AWS_CREDENTIALS_DIR"/credentials
 chmod 0600 "$AWS_CREDENTIALS_DIR"/credentials
-s3fs gisquick-backup "$MNT_S3_BUCKET_DIR"
+s3fs gisquick-backup:/qgis_server "$MNT_S3_BUCKET_DIR"
 mount -t nfs4 "$NFS_SERVER_ADDRESS":/ "$NFS_SERVER_MOUNT_DIR"
-tar -czf "$MNT_S3_BUCKET_DIR"/qgis-server/backup-qgis-server-publish-"$(date '+%Y-%m-%d')".tar.gz "$NFS_SERVER_MOUNT_DIR"/qgis-server-publish
-tar -czf "$MNT_S3_BUCKET_DIR"/qgis-server/backup-qgis-server-plugin-"$(date "+%Y-%m-%d")".tar.gz "$NFS_SERVER_MOUNT_DIR"/qgis-server-plugin
+cd "$MNT_S3_BUCKET_DIR"/qgis_server/
+tar -czf backup-qgis-server-publish-"$(date '+%Y-%m-%d')".tar.gz "$NFS_SERVER_MOUNT_DIR"/qgis-server-publish
+tar -czf backup-qgis-server-plugin-"$(date "+%Y-%m-%d")".tar.gz "$NFS_SERVER_MOUNT_DIR"/qgis-server-plugin
 umount "$MNT_S3_BUCKET_DIR"
